888977886c0207f2468f2247647b21ce5a35f338
jgirardet <ijkl@tuta.io>
2018-04-18 20:01:26 +0200

    coverage

--- tests/test_apistar_ponyorm.py
+++ tests/test_apistar_ponyorm.py
@@ -2,7 +2,7 @@
 import pony
 import pytest
 from apistar import App, Route, TestClient
-from pony.orm import Database, Required, TransactionError
+from pony.orm import Database, Required, TransactionError, db_session
 
 # apistar_ponyorm
 from apistar_ponyorm import PonyDBSession
@@ -30,18 +30,36 @@ def create() -> dict:
     return b.to_dict()
 
 
-route = Route(url="/", method="GET", handler=create)
+def server_error():
+    a = 1 / 0
+
+
+def server_error_db_session_already_closed():
+    db_session.__exit__()
+    a = 1 / 0
+
+
+route = [
+    Route(url="/", method="GET", handler=create),
+    Route(url="/zerodiv/", method="GET", handler=server_error),
+    Route(
+        url="/zerodiv_closed/",
+        method="GET",
+        handler=server_error_db_session_already_closed,
+    ),
+]
 
 
 def test_fail_without_on_request():
-    app = App(routes=[route])
+    app = App(routes=route)
     cli = TestClient(app)
     with pytest.raises(TransactionError) as e:
-        cli.get("/")
+        r = cli.get("/")
+        print(r.json())
     assert str(e.value) == "db_session is required when working with the database"
 
 
-app = App(routes=[route], event_hooks=[PonyDBSession()])
+app = App(routes=route, event_hooks=[PonyDBSession()])
 
 cli = TestClient(app)
 
@@ -65,3 +83,19 @@ def test_fail_if_not_found():
     r = cli.get("/fzefze/")
     r = cli.get("/")
     assert r.status_code == 200
+
+
+def test_server_error():
+    try:
+        r = cli.get("/zerodiv/")
+    except ZeroDivisionError:
+        pass
+    assert pony.orm.core.local.db_session is None
+
+
+def test_server_error_bd_already_closed():
+    try:
+        r = cli.get("/zerodiv_closed/")
+    except ZeroDivisionError:
+        pass
+    assert pony.orm.core.local.db_session is None
